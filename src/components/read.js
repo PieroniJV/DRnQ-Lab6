246d29c273cc3a621bdb3f8190a44fca217080b7
JOSE PIERONI DOS SANTOS - STUDENT <G00390007@atu.ie>
2022-11-11 10:49:18 +0000

    Second commit

--- src/components/read.js
+++ src/components/read.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Books } from "./books";
 import axios from "axios";
 
-export class Read extends React.Component{
-    
+export class Read extends React.Component{    
 
     componentDidMount() {
-        axios.get('https://jsonblob.com/api/jsonblob/1027219693823606784')
+        axios.get('http://localhost:4000/api/books')
         .then((response)=>{
-            this.setState({books:response.data})
+            this.setState({books:response.data.books})
         })
         .catch((error)=>{
             console.log(error);
